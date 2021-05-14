f245f2202599f3b334e81432a7154e94d1322b75
appketsu <79935072+appketsu@users.noreply.github.com>
2021-05-14 20:16:04 +0200

    Add files via upload

---
+++ designsjs.js
@@ -0,0 +1,86 @@
+const DefaultLayouts = {
+    ultraWideFull: 'ultraWideFull',
+    ultraWide: 'ultraWide',
+
+    wideFull: 'wideFull',
+    wide: 'wide',
+
+    wideStrechedFull: 'wideStrechedFull',
+    wideStrechedFullDouble: 'WideStrechedFullDouble',
+    wideStreched: 'wideStreched',
+    wideStrechedDouble: 'wideStrechedDouble',
+
+    wideStrechedFullList: 'wideStrechedFullList',
+    wideStrechedList: 'wideStrechedList',
+
+    doublets: 'doublets',
+    doubletsDouble: 'doubletsDouble',
+    doubletsFull: 'doubletsFull',
+    doubletsFullDouble: 'doubletsFullDouble',
+
+    doubletsConstant: 'doubletsConstant',
+    doubletsDoubleConstant: 'doubletsDoubleConstant',
+    doubletsFullConstant: 'doubletsFullConstant',
+    doubletsFullDoubleConstant: 'doubletsFullDoubleConstant',
+
+    longDoublets: 'longDoublets',
+    longDoubletsDouble: 'longDoubletsDouble',
+    longDoubletsFull: 'longDoubletsFull',
+    longDoubletsFullDouble: 'longDoubletsFullDouble',
+
+    longDoubletsConstant: 'longDoubletsConstant',
+    longDoubletsDoubleConstant: 'longDoubletsDoubleConstant',
+    longDoubletsFullConstant: 'longDoubletsFullConstant',
+    longDoubletsFullDoubleConstant: 'longDoubletsFullDoubleConstant',
+
+    triplets: 'triplets',
+    tripletsDouble: 'tripletsDouble',
+    tripletsFull: 'tripletsFull',
+    tripletsFullDouble: 'tripletsFullDouble',
+
+    tripletsConstant: 'tripletsConstant',
+    tripletsDoubleConstant: 'tripletsDoubleConstant',
+    tripletsFullConstant: 'tripletsFullConstant',
+    tripletsFullDoubleConstant: 'tripletsFullDoubleConstant',
+
+    longTriplets: 'longTriplets',
+    longTripletsDouble: 'longTripletsDouble',
+    longTripletsFull: 'longTripletsFull',
+    longTripletsFullDouble: 'longTripletsFullDouble',
+
+    longTripletsConstant: 'longTripletsConstant',
+    longTripletsDoubleConstant: 'longTripletsDoubleConstant',
+    longTripletsFullConstant: 'longTripletsFullConstant',
+    longTripletsFullDoubleConstant: 'longTripletsFullDoubleConstant',
+
+    none: ''
+};
+
+const CellDesings = {
+    Special1: 'Special1',
+    Special2: 'Special2',
+    Special3: 'Special3',
+    CELLHelperText: 'CELLHelperText',
+
+    small1: 'small1',
+    small2: 'small2',
+    normal1: 'normal1',
+    normal2: 'normal2',
+    normal3: 'normal3',
+    normal4: 'normal4',
+    normal5: 'normal5',
+    normal6: 'normal6',
+    normal7: 'normal7',
+
+    wide1: 'wide1',
+    wide2: 'wide2',
+    wide3: 'wide3',
+    wide4: 'wide4',
+    wide5: 'wide5',
+    wide6: 'wide6',
+    wide7: 'wide7',
+    wide8: 'wide8',
+    wide9: 'wide9',
+    wide10: 'wide10',
+    wide11: 'wide11'
+};
